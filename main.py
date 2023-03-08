1a9aaffc057143156be38c56ce7e217f222d62dc
yva <yvahajatiana@gmail.com>
2023-03-08 09:51:32 +0100

    feat: add gui

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from human import Person, Programmer
+from tkinter import *
 
-person = Person("Hajatiana", "Yva")
-person.showFullName()
-dev = Programmer("Hajatiana", "Yva")
-dev.setLanguage("TS, C#")
-dev.showProgrammerInformation()
+window = Tk()
+window.geometry('500x200')
+window.title('GUI')
+label = Label(window, text='Python Tkinter')
+label.pack(expand=1)
+frame = Frame(window, relief='ridge', borderwidth=20)
+frame.pack(fill='both', expand=1)
+window.mainloop()
